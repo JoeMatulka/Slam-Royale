a185ef39d63487f1c637f89d5fbc608c9e1f53e9
Joe Matulka <josef_matulka@hotmail.com>
2019-05-20 16:39:12 -0500

    Added Utility Folder

---
+++ Utility/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class Extensions
+{
+    public static Transform Search(this Transform target, string name)
+    {
+        if (target.name == name) return target;
+
+        for (int i = 0; i < target.childCount; ++i)
+        {
+            var result = Search(target.GetChild(i), name);
+
+            if (result != null) return result;
+        }
+
+        return null;
+    }
+}
