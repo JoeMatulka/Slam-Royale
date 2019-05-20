12cdfe7f38aa196bca3528fab8307e8eb4514cc2
Joe Matulka <josef_matulka@hotmail.com>
2019-05-20 16:27:23 -0500

    Added Player Code

---
+++ Player/Player.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerActionManager))]
+public class Player : Actor
+{
+    private const float HEALTH_RECOV_ON_BLOCK = 10f;
+
+    private const float INPUT_RUN_THRESHOLD = 0.95f;
+    private const float JUMP_DELAY = 0.1f;
+
+    private const float RUN_SPEED = 1f;
+    private const float WALK_SPEED = .5f;
+
+    private float x_Input, y_Input;
+
+    // Use this for initialization
+    void Start()
+    {
+        SetUpTargetLayer();
+
+        Animator = GetComponent<Animator>();
+        AudioSource = GetComponent<AudioSource>();
+        Rigidbody = GetComponent<Rigidbody>();
+        Collider = GetComponent<CapsuleCollider>();
+        HitBoxes = GetComponentsInChildren<Hitbox>();
+
+        ActionManager = GetComponent<ActionManager>();
+        EffectManager = GetComponent<ActorEffectsManager>();
+
+        InvokeRepeating("RecoverOvertime", 1f, RECOVER_RATE);
+
+        GameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    void FixedUpdate()
+    {
+        HandleControlInput();
+        Move(x_Input, y_Input, false);
+        SetBasicAnimationValues();
+    }
+
+    private void HandleControlInput()
+    {
+        x_Input = 0;
+        y_Input = 0;
+        if (ActionManager.Disabled)
+        {
+            return;
+        }
+        if (State.Equals(ActorState.DEFAULT) || State.Equals(ActorState.GRABBING) || State.Equals(ActorState.DOWN))
+        {
+            x_Input = CrossPlatformInputManager.GetAxis("Horizontal");
+            y_Input = CrossPlatformInputManager.GetAxis("Vertical");
+
+            // Throttle x input to manage proper acceleration
+            if (Mathf.Abs(x_Input) >= INPUT_RUN_THRESHOLD)
+            {
+                x_Input = x_Input > 0 ? RUN_SPEED : -RUN_SPEED;
+            }
+            else if (Mathf.Abs(x_Input) > 0)
+            {
+                x_Input = x_Input > 0 ? WALK_SPEED : -WALK_SPEED;
+            }
+        }
+    }
+
+
+    protected override void OnDamage()
+    {
+        SoundManager.Instance.Grunt(AudioSource);
+        ActionManager.OnDamage();
+    }
+
+    protected override void OnBlock()
+    {
+        // TODO, recover health and stun
+    }
+
+    protected override void OnGrabbed(Actor actor)
+    {
+        if (ActionManager.CurrentAttack != null &&
+        (ActionManager.CurrentAttack.AttackType.Equals(AttackType.STRIKE_GRAPPLE) ||
+         ActionManager.CurrentAttack.AttackType.Equals(AttackType.THROW_GRAPPLE)))
+        {
+            return;
+        }
+
+        ActionManager actorManager = actor.GetComponent<ActionManager>();
+
+        if (actorManager != null &&
+            actorManager.Grabbee == null)
+        {
+            if (Grounded)
+            {
+                SoundManager.Instance.SmallThud(AudioSource);
+            }
+            ActionManager.OnDamage();
+            ActionManager.Grabber = actor;
+            actorManager.Grabbee = this;
+        }
+    }
+
+    protected override void OnDestroy()
+    {
+
+    }
+
+    protected override void OnGrounded()
+    {
+        if (State.Equals(ActorState.ATTACKING) &&
+            ActionManager.CurrentAttack.AttackType.Equals(AttackType.STRIKE_GRAPPLE))
+        {
+            ActionManager.StrikeThrow();
+            ActionManager.CurrentAttack = null;
+        }
+        if (State.Equals(ActorState.GRABBING) && ActionManager.Grabbee != null)
+        {
+            // TODO: Set up a system to update values for grabbed actors
+            ActionManager.Grabbee.Grounded = true;
+        }
+    }
+
+    protected override void SetUpTargetLayer()
+    {
+        TargetLayer = 1 << LayerMask.NameToLayer("Default");
+    }
+}
---
+++ Player/PlayerActionManager.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerActionManager : ActionManager
+{
+    private GestureListener gestureListener;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<GameCamera>();
+
+        Actor = GetComponent<Actor>();
+
+        gestureListener = new GestureListener();
+        gestureListener.GestureEvent += new GestureListener.GestureEventHandler(HandleGesture);
+
+        atkManager = new WrestlerAttackManager(this);
+        sensorManager = new SensorManager(Actor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sensorManager.UpdateSensors();
+        if (!Disabled)
+        {
+            gestureListener.HandleTouchInput();
+        }
+        UpdateActionAnimations();
+
+        //Sanity check
+        if (Actor.State.Equals(ActorState.DEFAULT))
+        {
+            Grabbee = null;
+            Grabber = null;
+        }
+        // Clean up, can't figure why this isn't be cleared automatically
+        if (CurrentAttack != null && !Actor.State.Equals(ActorState.ATTACKING))
+        {
+            CurrentAttack = null;
+        }
+        if (Actor.State.Equals(ActorState.GRABBING) && Grabbee == null)
+        {
+            BreakGrab(false, false);
+        }
+        if (Actor.State.Equals(ActorState.GRABBED))
+        {
+            if (Grabber == null)
+            {
+                BreakGrab(true, false);
+            }
+            if (Grabber != null &&
+                Grabber.GetComponent<ActionManager>() != null &&
+                Grabber.GetComponent<ActionManager>().Grabbee == null)
+            {
+                BreakGrab(false, false);
+            }
+        }
+        if (Actor.State.Equals(ActorState.BEING_THROWN) &&
+    Grabber != null && Grabber.State.Equals(ActorState.DEFAULT))
+        {
+            BreakGrab(false, false);
+        }
+    }
+
+    private void HandleGesture(object sender, GestureEventArgs e)
+    {
+        switch (e.TouchPhase)
+        {
+            case TouchPhase.Began:
+                break;
+            case TouchPhase.Ended:
+                if (!CheckCanAttack())
+                {
+                    if (QueuedGesture == null) {
+                        QueuedGesture = e;
+                    }
+                    return;
+                }
+                TranslateGesture(e);
+                break;
+        }
+    }
+
+    private void TranslateGesture(GestureEventArgs e)
+    {
+        if (e.Gesture is GestureTap)
+        {
+            Actor.Rigidbody.velocity = Vector3.zero;
+            if (!Actor.State.Equals(ActorState.GRABBING))
+            {
+                Actor.Animator.Rebind();
+                CurrentAttack = atkManager.DetermineAttack(AttackType.HIT, Vector2.zero);
+            }
+            else {
+                BreakGrab(false, true);
+            }
+        }
+        else if (e.Gesture is GestureSwipe)
+        {
+            GestureSwipe gesture = e.Gesture as GestureSwipe;
+            switch (gesture.Direction)
+            {
+                case GestureSwipeDirection.UP:
+                    if (!Actor.State.Equals(ActorState.GRABBING))
+                    {
+                        Actor.Jump();
+                    }
+                    else {
+                        CurrentAttack = atkManager.DetermineAttack(AttackType.THROW_GRAPPLE, (gesture.EndLocation - gesture.StartLocation).normalized);
+                        Vector2 aim = CurrentAttack.Aim;
+                        if (aim.x >= 0)
+                        {
+                            Actor.Rigidbody.rotation = Actor.FACE_LEFT;
+                        }
+                        else {
+                            Actor.Rigidbody.rotation = Actor.FACE_RIGHT;
+                        }
+                    }
+                    break;
+                case GestureSwipeDirection.DOWN:
+                    if (!Actor.State.Equals(ActorState.GRABBING))
+                    {
+                        CurrentAttack = atkManager.DetermineAttack(AttackType.BLOCK, Vector2.zero);
+                    }
+                    else {
+                        CurrentAttack = atkManager.DetermineAttack(AttackType.STRIKE_GRAPPLE, Vector2.zero);
+                    }
+                    break;
+                case GestureSwipeDirection.LEFT:
+                    Actor.Rigidbody.rotation = Actor.FACE_RIGHT;
+                    if (!Actor.State.Equals(ActorState.GRABBING))
+                    {
+                        Actor.Animator.Rebind();
+                        CurrentAttack = atkManager.DetermineAttack(AttackType.GRAB, Vector2.zero);
+                    }
+                    else {
+                        CurrentAttack = atkManager.DetermineAttack(AttackType.THROW_GRAPPLE, (gesture.EndLocation - gesture.StartLocation).normalized);
+                    }
+                    break;
+                case GestureSwipeDirection.RIGHT:
+                    Actor.Rigidbody.rotation = Actor.FACE_LEFT;
+                    if (!Actor.State.Equals(ActorState.GRABBING))
+                    {
+                        Actor.Animator.Rebind();
+                        CurrentAttack = atkManager.DetermineAttack(AttackType.GRAB, Vector2.zero);
+                    }
+                    else {
+                        CurrentAttack = atkManager.DetermineAttack(AttackType.THROW_GRAPPLE, (gesture.EndLocation - gesture.StartLocation).normalized);
+                    }
+                    break;
+            }
+        }
+    }
+
+    public override void ActivateAttackFrame(int frame)
+    {
+        atkManager.ActivateAttackFrame(frame);
+    }
+
+    public override void OnDamage()
+    {
+        CurrentAttack = null;
+        QueuedGesture = null;
+        Actor.Invulnerable = false;
+        Actor.LockGravity = false;
+        atkManager.DeactivateHitboxes(Actor);
+    }
+
+    public override void ExecuteQueuedActions()
+    {
+        if (QueuedGesture != null) {
+            StartCoroutine(ExecuteQueuedGesture());
+        }
+    }
+
+    private IEnumerator ExecuteQueuedGesture() {
+        yield return new WaitUntil(() => Actor.State.Equals(ActorState.DEFAULT));
+        TranslateGesture(QueuedGesture);
+        QueuedGesture = null;
+    }
+}
